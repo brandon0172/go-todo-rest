7c2ceb045bfa3b846cbdb9b22b5b76d0af7d268e
Brandon0172 <brandonvanwijk@gmail.com>
2017-03-27 17:18:23 +0200

    add test method

--- main_test.go
+++ main_test.go
@@ -1,11 +1,20 @@
-package "main"
+package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func BenchmarkXxx(testing.B)
 
 func BenchmarkHello(b *testing.B) {
-    for i := 0; i < b.N; i++ {
-        fmt.Sprintf("hello")
-    }
+	for i := 0; i < b.N; i++ {
+		fmt.Sprintf("hello")
+	}
+}
+
+func Test() {
+	for i := 0; i < b.N; i++ {
+		fmt.Sprintf("hello")
+	}
 }
