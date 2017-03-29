0445c16024c183a0b9edfe2f850d90229b5d2a8a
Brandon0172 <brandonvanwijk@gmail.com>
2017-03-29 17:18:54 +0200

    add test

--- main_test.go
+++ main_test.go
@@ -1,24 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"testing"
-
-	"github.com/badmuts/go-todo-rest/web"
-	"github.com/stretchr/testify/assert"
 )
 
 func Test(t *testing.T) {
-	object := web.NewServer()
-
-	assert := assert.New(t)
-
-	assert.Equal(123, 123, "they should be equal")
-
-	assert.NotEqual(123, 456, "they should not be equal")
-
-	assert.NotNil(object)
-
-	if assert.NotNil(object) {
-		assert.Equal("Something", "Something")
+	for i := 1; i <= 5; i++ {
+		fmt.Printf("Welcome %d times\n", i)
 	}
 }
